683a5cdcddb9594a289d47bb33435a3dcd128038
shiropt <63380875+shiropt@users.noreply.github.com>
2025-01-26 20:34:09 +0900

    create IconWithText

---
+++ src/components/molecules/IconWithText.tsx
@@ -0,0 +1,34 @@
+import { Flex, Text } from "@mantine/core";
+import { FC } from "react";
+import { IconHome, IconMapPin, IconStar, IconUsers } from "@tabler/icons-react";
+
+type IconType = "home" | "mapPin" | "star" | "users";
+
+type Props = {
+  icon: IconType;
+  text: string;
+};
+
+const getIcon = (icon: IconType) => {
+  switch (icon) {
+    case "home":
+      return <IconHome color="gray" size={12} />;
+    case "mapPin":
+      return <IconMapPin color="gray" size={12} />;
+    case "star":
+      return <IconStar color="gray" size={12} />;
+    case "users":
+      return <IconUsers color="gray" size={12} />;
+  }
+};
+
+export const IconWithText: FC<Props> = ({ text, icon }) => {
+  return (
+    <Flex align="baseline" gap={4}>
+      {getIcon(icon)}
+      <Text fz="sm" c="gray.8">
+        {text}
+      </Text>
+    </Flex>
+  );
+};
