cc764f64ec50015d18a764d84c39bd861d3e8a1c
shiropt <63380875+shiropt@users.noreply.github.com>
2025-02-01 12:59:10 +0900

    modify Card

--- src/components/molecules/Card.tsx
+++ src/components/molecules/Card.tsx
@@ -1,25 +1,15 @@
 "use client";
 import { Image } from "@/components/atoms/Image";
 import { IconWithText } from "@/components/molecules/IconWithText";
-import { Badge, Flex, Grid, Paper, Title } from "@mantine/core";
+import { Stadium } from "@/utils/supabase/db/actions";
+import { Flex, Grid, Paper, Title } from "@mantine/core";
 import { FC } from "react";
-type Props = {
-  id: number;
-  created_at: string;
-  name: string;
-  homeTeam: string;
-  capacity: number;
-  access: string;
-  rating: number;
-  description: string;
-  location: string;
-  shortName: string;
-  imageUrl: string;
-};
+
+type Props = Stadium;
 
 export const Card: FC<Props> = ({
   name,
-  homeTeam,
+  homeTeams,
   capacity,
   access,
   rating,
@@ -27,9 +17,9 @@ export const Card: FC<Props> = ({
 }) => {
   return (
     <Grid.Col miw="280px" span={{ xs: 12, sm: 6, md: 4, lg: 3, xl: 2 }}>
-      <Paper withBorder radius="sm" p="sm">
+      <Paper radius="sm" p="sm">
         <Flex direction="column" gap="xs">
-          <Image alt="スタジアム画像" src={imageUrl} />
+          <Image alt="スタジアム画像" src={imageUrl ?? ""} />
           <Flex flex={1} justify="left" direction="column">
             <Flex align="center" justify="space-between">
               <Title fz="md" c="gray.9" order={3}>
@@ -37,20 +27,14 @@ export const Card: FC<Props> = ({
               </Title>
               <IconWithText text={`4.${rating}`} icon="star" />
             </Flex>
-            <IconWithText
-              text={`${capacity.toLocaleString()}人`}
-              icon="users"
-            />
-            <IconWithText text={homeTeam} icon="home" />
+            {capacity && (
+              <IconWithText
+                text={`${capacity.toLocaleString()}人`}
+                icon="users"
+              />
+            )}
+            <IconWithText text={homeTeams} icon="home" />
             <IconWithText text={access} icon="mapPin" />
-            <Flex mt="xs" mb="4" gap="xs">
-              <Badge variant="light" p="xs" color="blue.3">
-                屋内
-              </Badge>
-              <Badge variant="light" p="xs" color="blue.3">
-                駅近
-              </Badge>
-            </Flex>
           </Flex>
         </Flex>
       </Paper>
