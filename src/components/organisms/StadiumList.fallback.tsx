7d2605bda9f89cc42ec7890939703375bf775dc1
shiropt <63380875+shiropt@users.noreply.github.com>
2025-02-01 19:43:50 +0900

    create fallback component

---
+++ src/components/organisms/StadiumList.fallback.tsx
@@ -0,0 +1,14 @@
+import { FallbackCard } from "@/components/molecules/Card.fallback";
+import { Grid } from "@mantine/core";
+
+import { FC } from "react";
+
+export const FallbackStadiumList: FC = async () => {
+  return (
+    <Grid>
+      {[...new Array(6)].map((_, i) => {
+        return <FallbackCard key={i} />;
+      })}
+    </Grid>
+  );
+};
