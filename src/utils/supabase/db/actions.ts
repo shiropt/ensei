bcf482f33e9411922fcb003ccecc260c6c870f26
shiropt <63380875+shiropt@users.noreply.github.com>
2025-02-01 19:46:07 +0900

    modify getStadium

--- src/utils/supabase/db/actions.ts
+++ src/utils/supabase/db/actions.ts
@@ -2,8 +2,26 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const getStadiums = async () => {
+export type Params = {
+  category: "j1" | "j2" | "j3" | "all";
+};
+
+export const getStadiums = async ({ category }: Params) => {
   const stadiumsWithHomeTeams = await prisma.stadiums.findMany({
+    ...(category !== "all" && {
+      where: {
+        team_stadium: {
+          some: {
+            teams: {
+              category: {
+                equals: category,
+                mode: "insensitive",
+              },
+            },
+          },
+        },
+      },
+    }),
     include: {
       team_stadium: {
         include: {
