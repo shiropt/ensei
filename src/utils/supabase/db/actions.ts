04aa0b67ebfb5c0849edf735e6c620c4040a5ad9
shiropt <63380875+shiropt@users.noreply.github.com>
2025-02-02 09:42:12 +0900

    refactor actions

--- src/utils/supabase/db/actions.ts
+++ src/utils/supabase/db/actions.ts
@@ -6,22 +6,8 @@ export type Params = {
   category: "j1" | "j2" | "j3" | "all";
 };
 
-export const getStadiums = async ({ category }: Params) => {
-  const stadiumsWithHomeTeams = await prisma.stadiums.findMany({
-    ...(category !== "all" && {
-      where: {
-        team_stadium: {
-          some: {
-            teams: {
-              category: {
-                equals: category,
-                mode: "insensitive",
-              },
-            },
-          },
-        },
-      },
-    }),
+const getAllStadiums = async () => {
+  const stadiums = await prisma.stadiums.findMany({
     include: {
       team_stadium: {
         include: {
@@ -33,6 +19,42 @@ export const getStadiums = async ({ category }: Params) => {
       id: "asc",
     },
   });
+  return stadiums;
+};
+
+const getStadiumsByCategory = async (category: Params["category"]) => {
+  const stadiums = await prisma.stadiums.findMany({
+    where: {
+      team_stadium: {
+        some: {
+          teams: {
+            category: {
+              equals: category,
+              mode: "insensitive",
+            },
+          },
+        },
+      },
+    },
+    include: {
+      team_stadium: {
+        include: {
+          teams: true,
+        },
+      },
+    },
+    orderBy: {
+      id: "asc",
+    },
+  });
+  return stadiums;
+};
+
+export const getStadiums = async ({ category }: Params) => {
+  const stadiumsWithHomeTeams =
+    category === "all"
+      ? await getAllStadiums()
+      : await getStadiumsByCategory(category);
   return stadiumsWithHomeTeams.map((stadiumsWithHomeTeam) => {
     const { team_stadium, ...rest } = stadiumsWithHomeTeam;
     return {
