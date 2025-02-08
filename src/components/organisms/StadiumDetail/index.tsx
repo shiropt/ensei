d9bbcb60ba4a4ca16363c78b2e4a6938902f9a2b
shiropt <63380875+shiropt@users.noreply.github.com>
2025-02-08 15:17:50 +0900

    setting a responsive design

--- src/components/organisms/StadiumDetail/index.tsx
+++ src/components/organisms/StadiumDetail/index.tsx
@@ -1,7 +1,7 @@
 import { Image } from "@/components/atoms/Image";
 import { IconWithText } from "@/components/molecules/IconWithText";
 import { getStadium } from "@/utils/supabase/db/actions";
-import { Box, Title } from "@mantine/core";
+import { Box, Flex, Title } from "@mantine/core";
 import Link from "next/link";
 import { FC } from "react";
 
@@ -19,16 +19,43 @@ export const StadiumDetail: FC<Props> = async ({ id, from }) => {
       <Link href={from ?? "/"}>
         <IconWithText pt="xs" text="戻る" icon="arrowBackUp" />
       </Link>
-      <Title py="sm" fz="lg" order={2}>
-        {name}
-      </Title>
-      <Image src={imageUrl ?? ""} radius="none" alt=""></Image>
-      <Box py="sm">
-        {capacity && (
-          <IconWithText text={`${capacity.toLocaleString()}人`} icon="users" />
-        )}
-        <IconWithText text={homeTeams} icon="home" />
-        <IconWithText text={access} icon="mapPin" />
+      <Box className="stadium-detail__mobile">
+        <Title py="sm" fz="lg" order={2}>
+          {name}
+        </Title>
+        <Box>
+          <Image src={imageUrl ?? ""} radius="none" alt=""></Image>
+        </Box>
+        <Box py="sm">
+          {capacity && (
+            <IconWithText
+              text={`${capacity.toLocaleString()}人`}
+              icon="users"
+            />
+          )}
+          <IconWithText text={homeTeams} icon="home" />
+          <IconWithText text={access} icon="mapPin" />
+        </Box>
+      </Box>
+      <Box py="sm" className="stadium-detail__desktop">
+        <Flex gap="md">
+          <Box w="50%">
+            <Image src={imageUrl ?? ""} radius="none" alt=""></Image>
+          </Box>
+          <Box>
+            <Title fz="lg" order={2}>
+              {name}
+            </Title>
+            {capacity && (
+              <IconWithText
+                text={`${capacity.toLocaleString()}人`}
+                icon="users"
+              />
+            )}
+            <IconWithText text={homeTeams} icon="home" />
+            <IconWithText text={access} icon="mapPin" />
+          </Box>
+        </Flex>
       </Box>
     </Box>
   );
