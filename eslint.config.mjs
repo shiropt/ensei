94d8985af6ecf5f3204e24e8a2dc36a216a2c71a
shiropt <63380875+shiropt@users.noreply.github.com>
2025-03-08 17:50:55 +0900

    chore: add tslint monorepo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,8 @@
 import pluginJs from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
+import functional from "eslint-plugin-functional";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
@@ -12,7 +15,35 @@ const nextConfig = [
   }),
 ];
 
-/** @type {import('eslint').Linter.Config[]} */
-const config = [...nextConfig, pluginJs.configs.recommended];
+/** @type {import('eslint').Linter.FlatConfig[]} */
+const config = [
+  ...nextConfig,
+  pluginJs.configs.recommended,
+  // functional.configs.externalTypeScriptRecommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      functional,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...functional.configs.externalTypeScriptRecommended.rules,
+      ...functional.configs.recommended.rules,
+      ...functional.configs.stylistic.rules,
+      "functional/prefer-immutable-types": "off",
+      "functional/no-return-void": "off",
+      "functional/functional-parameters": "off",
+      "functional/no-conditional-statement": "off",
+      "functional/no-expression-statements": "off",
+    },
+  },
+];
 
 export default config;
